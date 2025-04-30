3756e021e99e1c8ae9c1b8b54957a82386b7410f
vano <vanuartw@mail.ru>
2025-04-30 14:28:35 +0300

    Создание модели машины

--- car_showroom/cars/models.py
+++ car_showroom/cars/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from users.models import Profile
 
-# Create your models here.
+
+class Car(models.Model):
+    CHOICES = [
+        ('S', 'sedan'),
+        ('M', 'minivan'),
+        ('H', 'hatchback')
+    ]
+
+    profile_user = models.ForeignKey(
+        Profile,
+        related_name='profile_cars',
+        on_delete=models.CASCADE
+    )
+    car_type = models.CharField(
+        max_length=1,
+        blank=True,
+        null=True,
+        choices=CHOICES
+    )
+    brand = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    year = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return f'Машина {self.name} пользователя {self.profile_user.user}'
