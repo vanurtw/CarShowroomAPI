939fcb588a477c7a3836ae88558e4de0a3b14a2b
Ivan <vanuartw@mail.ru>
2025-05-01 15:53:02 +0300

    Создание и регистрация модели заказа

--- car_showroom/cars/admin.py
+++ car_showroom/cars/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Record
 
 
 @admin.register(Car)
 class CarAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Record)
+class RecordAdmin(admin.ModelAdmin):
     pass
